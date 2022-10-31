6db5851e21a7099ed6f009b6815e2f93019a973f
biprateep <biprateep@pitt.edu>
2022-10-31 16:06:52 -0700

    expose new functions

--- py/desigal/specutils/__init__.py
+++ py/desigal/specutils/__init__.py
@@ -1,4 +1,6 @@
-from .redshift import redshift
+from .redshift import deredshift
 from .coadd_cameras import coadd_cameras
 from .spectra_io import get_spectra
 from .sky import get_sky
+from .resample import resample
+from .normalize import normalize
