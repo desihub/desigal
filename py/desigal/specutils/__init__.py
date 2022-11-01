78d1f50ba2528b9c4e1fbafe74d991a57419cc7b
biprateep <biprateep@pitt.edu>
2022-10-31 22:09:30 -0700

    expose new functions

--- py/desigal/specutils/__init__.py
+++ py/desigal/specutils/__init__.py
@@ -4,3 +4,5 @@ from .spectra_io import get_spectra
 from .sky import get_sky
 from .resample import resample
 from .normalize import normalize
+from .coaddition import coadd_flux
+from .stack import stack_spectra
