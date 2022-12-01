30f469e10c22d07c10e0d1fc000c6a5017490144
Dirk Scholte <dirk.scholte.20@ucl.ac.uk>
2022-12-01 03:10:46 -0800

    Add ivar-weighted-average and spec-weighted-average

--- py/desigal/specutils/coaddition.py
+++ py/desigal/specutils/coaddition.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 
-def coadd_flux(wave, flux, ivar, mask=None, method="mean", n_workers=1):
+def coadd_flux(wave, flux, ivar, mask=None, method="mean", weight=None, n_workers=1):
     """ Coadd spectra using S/N weighted mean. """
-    methods = ["mean", "median", "ivar-weighted-mean"]
+    methods = ["mean", "median", "ivar-weighted-mean", "spec-weighted-mean"]
     if method not in methods:
         raise ValueError(f"Coadd type must be one of {methods}")
     if method == "mean":
@@ -11,4 +11,14 @@ def coadd_flux(wave, flux, ivar, mask=None, method="mean", n_workers=1):
     elif method == "median":
         return np.nanmedian(flux, axis=0)
     elif method == "ivar-weighted-mean":
-        raise NotImplementedError
+        weight = ivar
+        nan_mask = (np.isnan(flux)) | (np.isnan(weight))
+        flux[nan_mask] = 0.0
+        weight[nan_mask] = 0.0
+        return np.average(flux, weights=weight, axis=0)
+    elif method == "spec-weighted-mean":
+        if np.any(weight==None):
+            raise ValueError(f"Weight must be specified when performing a weighted average.")
+        nan_mask = (np.isnan(flux))
+        #flux[nan_mask] = 0.0
+        return np.average(flux, weights=weight, axis=0)
