7c252a775b8df440dc253a50a9540718c73a9e47
Dirk Scholte <dirk.scholte.20@ucl.ac.uk>
2023-04-11 08:59:03 -0700

    First working version with propagated errors

--- py/desigal/specutils/coaddition.py
+++ py/desigal/specutils/coaddition.py
@@ -16,7 +16,9 @@ def _coadd_flux(wave, flux, ivar, mask=None, method="mean", weight=None):
         wl_weight[nan_mask] = 1e-10 # hack
         stacked_flux = np.average(flux, weights=wl_weight, axis=0)
         stacked_flux[np.sum(~nan_mask, axis=0)==0] = np.nan # only allow wl which is covered by at least 1 spectrum
-        return stacked_flux
+        stacked_ivar = np.average(ivar**-2, weights=wl_weight**2, axis=0)**-1
+        stacked_ivar[np.sum(~nan_mask, axis=0)==0] = np.nan # only allow wl which is covered by at least 1 spectrum
+        return np.stack([stacked_flux, stacked_ivar], axis=-1)
     elif method == "median":
         return np.nanmedian(flux, axis=0)
     elif method == "ivar-weighted-mean":
@@ -26,7 +28,9 @@ def _coadd_flux(wave, flux, ivar, mask=None, method="mean", weight=None):
         wl_weight[nan_mask] = 1e-10 # hack
         stacked_flux = np.average(flux, weights=wl_weight, axis=0)
         stacked_flux[np.sum(~nan_mask, axis=0)==0] = np.nan # only allow wl which is covered by at least 1 spectrum
-        return stacked_flux
+        stacked_ivar = np.average(ivar**-2, weights=wl_weight**2, axis=0)**-1
+        stacked_ivar[np.sum(~nan_mask, axis=0)==0] = np.nan # only allow wl which is covered by at least 1 spectrum
+        return np.stack([stacked_flux, stacked_ivar], axis=-1)
     elif method == "irms-weighted-mean":
         wl_weight = ivar**0.5 * np.expand_dims(weight, axis=-1)
         nan_mask = (np.isnan(flux)) | (np.isnan(ivar)) | (ivar==0.) | (np.isnan(wl_weight))
@@ -34,7 +38,9 @@ def _coadd_flux(wave, flux, ivar, mask=None, method="mean", weight=None):
         wl_weight[nan_mask] = 1e-10 # hack
         stacked_flux = np.average(flux, weights=wl_weight, axis=0)
         stacked_flux[np.sum(~nan_mask, axis=0)==0] = np.nan # only allow wl which is covered by at least 1 spectrum
-        return stacked_flux
+        stacked_ivar = np.average(ivar**-2, weights=wl_weight**2, axis=0)**-1
+        stacked_ivar[np.sum(~nan_mask, axis=0)==0] = np.nan # only allow wl which is covered by at least 1 spectrum
+        return np.stack([stacked_flux, stacked_ivar], axis=-1)
 
 def bootstrap_coadd(wave, flux, ivar, mask=None, method="mean", weight=None):
     boot_idx = np.random.choice(
@@ -42,7 +48,7 @@ def bootstrap_coadd(wave, flux, ivar, mask=None, method="mean", weight=None):
     )  # take a random sample each iteration
     boot_coadd = _coadd_flux(
         wave, 
-        flux[boot_idx]+np.random.normal(size=ivar[boot_idx].shape)*ivar[boot_idx]**-0.5, 
+        flux[boot_idx],#+np.random.normal(size=ivar[boot_idx].shape)*ivar[boot_idx]**-0.5, 
         ivar[boot_idx], 
         mask=mask, 
         method=method, 
@@ -66,12 +72,13 @@ def coadd_flux(
 ):
     if np.all(weight==None):
         weight = np.ones(len(flux))
-    stack_errors = ["no-errors", "bootstrap"]
+    stack_errors = ["propagated", "bootstrap"]
     if stack_error not in stack_errors:
         raise ValueError(f"Coadd type must be one of {stack_errors}")
-    if stack_error == "no-errors":
+    if stack_error == "propagated":
         return _coadd_flux(wave, flux, ivar, mask=mask, method=method, weight=weight)
     if stack_error == "bootstrap":
+        print('start bootstrapping!')
         if n_workers == 1:
             boot_averages = np.array(
                 [
@@ -81,7 +88,8 @@ def coadd_flux(
                     for _ in range(bootstrap_samples)
                 ]
             )
-            return np.nanmean(boot_averages, axis=0), np.nanvar(boot_averages, axis=0)**-1
+            print(boot_averages.shape)
+            return np.nanmean(boot_averages[:,:,0], axis=0), np.nanmean(boot_averages[:,:,1], axis=0)
         else:
             boot_averages = np.array(
                 Parallel(n_jobs=n_workers)(
@@ -91,4 +99,5 @@ def coadd_flux(
                     for _ in range(bootstrap_samples)
                 )
             )
-            return np.nanmean(boot_averages, axis=0), np.nanvar(boot_averages, axis=0)**-1
+            print(boot_averages.shape)
+            return np.nanmean(boot_averages[:,:,0], axis=0), np.nanmean(boot_averages[:,:,1], axis=0)
